68811ff7821410f72383f98c4ffcb688872f4f5a
Mark Shurmer <mark.shurmer@interactconsultants.co.uk>
2023-05-03 12:52:57 +0100

    add tests for results

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,19 +1,22 @@
 import { MutableSnapshot } from 'recoil';
-import { getMockQuestions, render, screen } from '../setup/test-helpers';
+import { getMockQuestions, render, screen, waitFor } from '../setup/test-helpers';
 import { gameStatusAtom, gameActivityAtom } from './app-state/atoms';
 import { getStep } from './app-state/state-machine';
 import App from './App';
-import { test, expect, vi } from 'vitest';
+import { test, expect } from 'vitest';
+import { getGameInfo } from './app-state/game-info';
 
 const info = getMockQuestions();
 const initializeState = ({ set }: MutableSnapshot) => {
     set(gameStatusAtom, getStep(1, 7));
     set(gameActivityAtom, info.activities[0]);
 };
-test('App - shows home page when step type is Start', () => {
+test('App - shows home page when step type is Start', async () => {
     render(<App />);
 
-    expect(screen.getByRole('main')).toBeDefined();
+    await waitFor(() => {
+        expect(screen.getByRole('main')).toBeDefined();
+    });
 });
 
 test('App - shows home page when step type is Start with results', () => {
