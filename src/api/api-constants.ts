3411b9060e4517f9d1275e0f35a215b1dcf378d3
MarkShurmer <mark@interactconsultants.co.uk>
2023-05-03 16:28:02 +0100

    Deploy (#14)
    * change action to pull request
    
    * update name for deploy job
    
    * try again
    
    * change env setting
    
    * update url to use our own
    
    * remove spurious text
    
    ---------
    
    Co-authored-by: Mark Shurmer <mark.shurmer@interactconsultants.co.uk>

--- src/api/api-constants.ts
+++ src/api/api-constants.ts
@@ -1,3 +1,2 @@
-export const API_URL =
-  'https://s3.eu-west-2.amazonaws.com/interview.mock.data/payload.json';
+export const API_URL = 'http://multi-quiz.s3-website.eu-west-2.amazonaws.com/data.json';
 export const API_URL_DEV = 'http://localhost:5173/data.json';
