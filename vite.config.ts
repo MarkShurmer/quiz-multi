347ba7dc60d47ba579ee303e0e32c40dc51f9ecf
Mark Shurmer <mark.shurmer@interactconsultants.co.uk>
2023-05-03 14:44:15 +0100

    add deploy for s3

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    build: {
+        target: 'esnext',
+    },
     test: {
         environment: 'jsdom',
         setupFiles: './setup/setup-tests.ts',
